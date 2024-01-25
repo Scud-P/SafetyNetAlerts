f51a7b54f00080fc6fa1a898812b86caa03e3722
PScud <escudie.pierre@gmail.com>
2024-01-25 15:16:30 -0500

    Deleted custom exceptions

--- src/main/java/com/oc/safetynet/alertsapi/exception/FireStationNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.oc.safetynet.alertsapi.exception;
-
-public class FireStationNotFoundException extends RuntimeException {
-
-    public FireStationNotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/oc/safetynet/alertsapi/exception/PersonAlreadyInDBException.java
+++
@@ -1,2 +0,0 @@
-package com.oc.safetynet.alertsapi.exception;public class PersonAlreadyInDBException {
-}
